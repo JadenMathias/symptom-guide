ade6ac79b83ee65db9489c9d194d2c44fe06976e
JadenMathias <mathias.jaden@gmail.com>
2022-06-05 14:40:46 -0400

    web version

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,59 +2,57 @@ import * as React from "react";
 import classes from "./Header.module.scss";
 import { BiMenuAltRight } from "react-icons/bi";
 import { AiOutlineClose } from "react-icons/ai";
-import img from "./smtp.png";
+import img from "../../public/images/smtp.png";
 
 const Header = () => {
-  const [menuOpen, setMenuOpen] = React.useState(false);
-  const [size, setSize] = React.useState({
-    width: undefined,
-    height: undefined,
-  });
+	const [menuOpen, setMenuOpen] = React.useState(false);
+	const [size, setSize] = React.useState({
+		width: undefined,
+		height: undefined,
+	});
 
-  React.useEffect(() => {
-    const handleResize = () => {
-      setSize({ width: window.innerWidth, height: window.innerHeight });
-    };
-    window.addEventListener("resize", handleResize);
-    return () => window.removeEventListener("resize", handleResize);
-  }, []);
+	React.useEffect(() => {
+		const handleResize = () => {
+			setSize({ width: window.innerWidth, height: window.innerHeight });
+		};
+		window.addEventListener("resize", handleResize);
+		return () => window.removeEventListener("resize", handleResize);
+	}, []);
 
-  React.useEffect(() => {
-    if (size.width > 768 && menuOpen) {
-      setMenuOpen(false);
-    }
-  }, [size.width, menuOpen]);
+	React.useEffect(() => {
+		if (size.width > 768 && menuOpen) {
+			setMenuOpen(false);
+		}
+	}, [size.width, menuOpen]);
 
-  const menuToggle = () => {
-    setMenuOpen(!menuOpen);
-  };
+	const menuToggle = () => {
+		setMenuOpen(!menuOpen);
+	};
 
-  return (
-    <header className={classes.header}>
-      <div className={classes.header__content}>
-        <img className={classes.header__content__logo} src={img}></img>
-        <nav
-          className={`${classes.header__content__nav} ${
-            menuOpen ? classes.isMenu : ""
-          }`}
-        >
-          <ul>
-            <li>
-              <a href="/">Home</a>
-            </li>
-          </ul>
-          <button>Check Symptoms</button>
-        </nav>
-        <div className={classes.header__content__toggle}>
-          {menuOpen ? (
-            <AiOutlineClose onClick={menuToggle} />
-          ) : (
-            <BiMenuAltRight onClick={menuToggle} />
-          )}
-        </div>
-      </div>
-    </header>
-  );
+	return (
+		<header className={classes.header}>
+			<div className={classes.header__content}>
+				<img className={classes.header__content__logo} src={img}></img>
+				<nav
+					className={`${classes.header__content__nav} ${menuOpen ? classes.isMenu : ""}`}
+				>
+					<ul>
+						<li>
+							<a href="/">Home</a>
+						</li>
+					</ul>
+					<button>Check Symptoms</button>
+				</nav>
+				<div className={classes.header__content__toggle}>
+					{menuOpen ? (
+						<AiOutlineClose onClick={menuToggle} />
+					) : (
+						<BiMenuAltRight onClick={menuToggle} />
+					)}
+				</div>
+			</div>
+		</header>
+	);
 };
 
 export default Header;
--- webpack.config.js
+++ webpack.config.js
@@ -1,41 +1,41 @@
 const path = require("path");
 module.exports = {
-  entry: "./src/index.tsx",
-  output: {
-    filename: "bundle.js",
-    path: path.resolve(__dirname, "dist"),
-  },
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        use: "ts-loader",
-        exclude: /node_modules/,
-      },
-      {
-        test: /\.css?$/,
-        use: ["style-loader", "css-loader"],
-        exclude: /node_modules/,
-      },
-      {
-        test: /\.s[ac]ss$/i,
-        use: ["style-loader", "css-loader", "sass-loader"],
-        exclude: /node_modules/,
-      },
-      {
-        test: /\.(png|jpe?g|gif)$/i,
-        use: [
-          {
-            loader: "file-loader",
-          },
-        ],
-        exclude: /node_modules/,
-      },
-    ],
-  },
-  resolve: {
-    extensions: [".ts", ".tsx", ".js", ".scss", ".css", ".png"],
-  },
-  watch: true,
-  mode: "development",
+	entry: "./src/index.tsx",
+	output: {
+		filename: "bundle.js",
+		path: path.resolve(__dirname, "build"),
+	},
+	module: {
+		rules: [
+			{
+				test: /\.tsx?$/,
+				use: "ts-loader",
+				exclude: /node_modules/,
+			},
+			{
+				test: /\.css?$/,
+				use: ["style-loader", "css-loader"],
+				exclude: /node_modules/,
+			},
+			{
+				test: /\.s[ac]ss$/i,
+				use: ["style-loader", "css-loader", "sass-loader"],
+				exclude: /node_modules/,
+			},
+			{
+				test: /\.(png|jpe?g|gif)$/i,
+				use: [
+					{
+						loader: "file-loader",
+					},
+				],
+				exclude: /node_modules/,
+			},
+		],
+	},
+	resolve: {
+		extensions: [".ts", ".tsx", ".js", ".scss", ".css", ".png"],
+	},
+	watch: false,
+	mode: "development",
 };
